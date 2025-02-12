c4ccd9b9acacc8b9eefcfe31a42d69c54a1a35b0
Harsh Dixit <143201960+harshdixit3@users.noreply.github.com>
2025-02-12 21:07:16 +0530

    first commit

---
+++ components/Camp.tsx
@@ -0,0 +1,9 @@
+const Camp = () => {
+  return (
+    <section className="border-2 *:border-red-500 2xl:max-container relative lex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20">
+      <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:"></div>
+    </section>
+  );
+};
+
+export default Camp;
